bd8d2329d977357533c1e5db0e75b3b07534e94a
Brent Mercado <brent.mercado.566@my.csun.edu>
2018-02-12 14:43:03 -0800

    Commented out examples

--- routes/app.js
+++ routes/app.js
@@ -1,32 +1,33 @@
 var express = require('express');
 var router = express.Router();
-const User = require('../models/user');
+// const User = require('../models/user');
 
 router.get('/', function (req, res, next) {
+   res.render('index');
    // fetch data from the database
-   User.findOne({}, (err,doc) => {
-      if (err) {
-         return res.send('Error!')
-      }
-      res.render('node', {email: doc.email});
-   });
+   // User.findOne({}, (err,doc) => {
+   //    if (err) {
+   //       return res.send('Error!')
+   //    }
+   //    res.render('node', {email: doc.email});
+   // });
 });
 
 // router.get('/message/:msg', (req, res, next) => {
 //    res.render('node', {message: req.params.msg});
 // });
 
-router.post('/', (req, res, next) => {
-   var email = req.body.email;
-   var user = new User({
-      firstName: 'Brent',
-      lastName: 'Mercado',
-      password: 'super-secret',
-      email: email
-   });
-   // save to the database
-   user.save();
-   res.redirect('/');
-});
+// router.post('/', (req, res, next) => {
+//    var email = req.body.email;
+//    var user = new User({
+//       firstName: 'Brent',
+//       lastName: 'Mercado',
+//       password: 'super-secret',
+//       email: email
+//    });
+//    // save to the database
+//    user.save();
+//    res.redirect('/');
+// });
 
 module.exports = router;
