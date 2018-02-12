60772630156f0536148e563675ba3d1939fcef2e
Brent Mercado <brent.mercado.566@my.csun.edu>
2018-02-11 16:53:17 -0800

    Added a .post route to the /message url

--- routes/app.js
+++ routes/app.js
@@ -5,8 +5,13 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
-router.get('/message', (req, res, next) => {
-   res.render('node', {message: 'Hello!'});
+router.get('/message/:msg', (req, res, next) => {
+   res.render('node', {message: req.params.msg});
+});
+
+router.post('/message', (req, res, next) => {
+   var message = req.body.message;
+   res.redirect('/message/' + message);
 });
 
 module.exports = router;
