40fc0fb0d41fccc57353343840ef00a75bcb1cff
Brent Mercado <brent.mercado.566@my.csun.edu>
2018-02-11 17:57:21 -0800

    Store a user in the database

--- models/user.js
+++ models/user.js
@@ -14,6 +14,6 @@ const UserSchema = new Schema({
 });
 
 // allows us to use the unique validator
-schema.plugin(mongooseUniqueValidator);
+UserSchema.plugin(mongooseUniqueValidator);
 
 module.exports = mongoose.model('User', UserSchema);
--- routes/app.js
+++ routes/app.js
@@ -1,17 +1,26 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+    res.render('node');
 });
 
-router.get('/message/:msg', (req, res, next) => {
-   res.render('node', {message: req.params.msg});
-});
+// router.get('/message/:msg', (req, res, next) => {
+//    res.render('node', {message: req.params.msg});
+// });
 
-router.post('/message', (req, res, next) => {
-   var message = req.body.message;
-   res.redirect('/message/' + message);
+router.post('/', (req, res, next) => {
+   var email = req.body.email;
+   var user = new User({
+      firstName: 'Brent',
+      lastName: 'Mercado',
+      password: 'super-secret',
+      email: email
+   });
+   // save to the database
+   user.save();
+   res.redirect('/');
 });
 
 module.exports = router;
