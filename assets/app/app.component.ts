11c9b6883d7e5608baf0b52d2674b227e3ea05a0
Brent Mercado <brent.mercado.566@my.csun.edu>
2018-02-13 21:27:39 -0800

    Added input and list components

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,15 +1,9 @@
 import { Component } from '@angular/core';
 
-import { Message } from "./message/message.model";
-
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html'
 })
 export class AppComponent {
-   messages: Message[] = [
-      new Message('Some message', 'Brent'),
-      new Message('Someting else', 'Brent'),
-      new Message('3rd message!', 'Brent')
-   ];
+
 }
--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -4,11 +4,15 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { MessageComponent } from "./message/message.component";
+import { MessageListComponent } from "./message/message-list.component";
+import { MessageInputComponent } from "./message/message-input.component";
 
 @NgModule({
     declarations: [
         AppComponent,
-        MessageComponent
+        MessageComponent,
+        MessageListComponent,
+        MessageInputComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
