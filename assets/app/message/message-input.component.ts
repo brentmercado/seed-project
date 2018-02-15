99b43b8c4f9e26744b0e476477e373074264ce30
Brent Mercado <brent.mercado.566@my.csun.edu>
2018-02-14 18:15:52 -0800

    Adding Messages with a Service

--- assets/app/message/message-input.component.ts
+++ assets/app/message/message-input.component.ts
@@ -1,12 +1,19 @@
 import { Component } from "@angular/core";
 
+import { Message } from "./message.model";
+import { MessageService } from "./message.service";
+
 @Component({
    selector: 'app-message-input',
-   templateUrl: './message-input.component.html'
+   templateUrl: './message-input.component.html',
+   providers: [MessageService]
 })
 
 export class MessageInputComponent {
+
+   constructor(private messageService: MessageService) {}
    onSave(value: string) {
-      console.log(value);
+      const message = new Message(value, 'Brent');
+      this.messageService.addMessage(message);
    }
 }
